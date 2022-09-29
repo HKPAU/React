2bfd408193368aeaf8c4bc39d31d357836bed7b7
hasankacar <hasan.kacar@kodpit.com>
2022-09-29 22:56:31 +0300

    Initial Push

---
+++ intro/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navi from "./Components/navi";
+import ProductList from "./Components/productList";
+import CategoryList from "./Components/categoryList";
+
+function App() {
+  return (
+    <div>
+        <Navi/>
+        <ProductList/>
+        <CategoryList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ intro/src/Components/navi.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hasan Kacar - Pamukkale University - Computer Engineering</h3>
+            </div>
+        );
+    }
+}
+
+export default Navi;
